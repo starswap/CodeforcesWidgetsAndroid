4564173c1da502417da5df2057ac6b22a9ff8805
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-12-29 11:19:06 +0000

    Clean up code

--- app/src/main/java/com/starswap/codeforceswidgets/TimerWidgetProvider.kt
+++ app/src/main/java/com/starswap/codeforceswidgets/TimerWidgetProvider.kt
@@ -15,7 +15,6 @@ class TimerWidgetProvider : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        Log.d("Getting called now", "yeeeee");
         // Perform this loop procedure for each widget that belongs to this
         // provider.
         appWidgetIds.forEach { appWidgetId ->
@@ -28,9 +27,6 @@ class TimerWidgetProvider : AppWidgetProvider() {
 //                /* flags = */ PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
 //            )
 //
-//            // Get the layout for the widget and attach an onClick listener to
-//            // the button
-            Log.d("Hopp", "hoppp")
             val remoteViews = RemoteViews(context.packageName, R.layout.timer_widget_start_layout)
             remoteViews.setChronometerCountDown(R.id.chronometer, false);
             remoteViews.setChronometer(R.id.chronometer, SystemClock.elapsedRealtime() - 50000, null, true)
