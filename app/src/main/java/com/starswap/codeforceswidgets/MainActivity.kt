210ea55f831ca2c71a9f8821518cde14244c1c78
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-01-04 15:07:33 +0000

    Implement basic initial activity

--- app/src/main/java/com/starswap/codeforceswidgets/MainActivity.kt
+++ app/src/main/java/com/starswap/codeforceswidgets/MainActivity.kt
@@ -15,14 +15,7 @@ import com.starswap.codeforceswidgets.ui.theme.CodeforcesWidgetsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CodeforcesWidgetsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main);
     }
 }
 
