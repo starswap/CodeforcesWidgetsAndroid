ad36ead5fdc0b05214e2627e1a32b1f6becf4820
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-01-05 11:08:29 +0000

    Implement base 6 tally display for solved problems

--- app/src/main/java/com/starswap/codeforceswidgets/streakwidget/StreakWidgetProvider.kt
+++ app/src/main/java/com/starswap/codeforceswidgets/streakwidget/StreakWidgetProvider.kt
@@ -3,13 +3,11 @@ package com.starswap.codeforceswidgets.streakwidget
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.net.Uri
-import android.os.SystemClock
+import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.RemoteViews
 import com.starswap.codeforceswidgets.R
-import com.starswap.codeforceswidgets.codeforces.get_user
-import com.starswap.codeforceswidgets.codeforces.latest_submissions
-import com.starswap.codeforceswidgets.codeforces.render_user
 import com.starswap.codeforceswidgets.handle.loadHandle
 
 class StreakWidgetProvider : AppWidgetProvider() {
@@ -22,14 +20,34 @@ class StreakWidgetProvider : AppWidgetProvider() {
     }
 
     companion object {
+        private val number_to_tally_image = mapOf(
+            1 to R.drawable.tally_one,
+            2 to R.drawable.tally_two,
+            3 to R.drawable.tally_three,
+            4 to R.drawable.tally_four,
+            5 to R.drawable.tally_five
+        )
+
+        private fun updateTallyChart(remoteViews: RemoteViews, tallyChart: Int, number: Int) {
+            number_to_tally_image[number]?.let {
+                remoteViews.setViewVisibility(tallyChart, VISIBLE)
+                remoteViews.setImageViewResource(tallyChart, it)
+            } ?: run {
+                remoteViews.setViewVisibility(tallyChart, GONE)
+            }
+        }
+
         fun updateOneWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
             val handle = loadHandle(context, appWidgetId)
             if (handle != null) {
                 val remoteViews = RemoteViews(context.packageName, R.layout.streak_widget_start_layout)
-                val (streak, doneToday) = Pair(10, false)// streak(handle);
+                val (streak, doneToday) = Pair(10, 100)// streak(handle);
 
                 remoteViews.setTextViewText(R.id.streak, streak.toString())
-                remoteViews.setImageViewResource(R.id.cf_logo, if (doneToday) R.drawable.codeforces_logo_colour else R.drawable.codeforces_logo_bw)
+                remoteViews.setImageViewResource(R.id.cf_logo, if (doneToday > 0) R.drawable.codeforces_logo_colour else R.drawable.codeforces_logo_bw)
+
+                updateTallyChart(remoteViews, R.id.tally_chart_red, doneToday / 6)
+                updateTallyChart(remoteViews, R.id.tally_chart, doneToday % 6)
                 appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
             }
         }
